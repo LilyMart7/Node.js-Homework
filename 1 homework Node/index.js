366b67f5551db40541a15689e39dce763ad37edb
LilyMart7 <lilymart7@gmail.com>
2019-06-06 18:33:56 +0300

    updated Help section

--- 1 homework Node/index.js
+++ 1 homework Node/index.js
@@ -5,56 +5,61 @@ const Toy = require('./classes/Toy');
 
 const argv = process.argv.slice(2);
 
-switch(argv[0]){
+switch (argv[0]) {
     case `--help`:
-        console.log(`--help Show all info;`);
+        console.log(`Show all info:`);
+        console.log(`--create: put USER's information in such order => Name, Password`);
+        console.log(`--generate: put DOG's information in such order => First name, Breed, Age`);
+        console.log(`--produce: put CAT's information in such order => Nickname, Color, number of Siblings`);
+        console.log(`--discover: put TOY's information in such order => Type, Material, Size`);
         break;
 
-        case `--create`:
-            let name = argv[1];
-            let pass = argv[2];
-            let users = new User(name, pass);
-            users.ShowUser();
+    case `--h`:
+        console.log(`Show all info:`);
+        console.log(`--create: put USER's information in such order => Name, Password`);
+        console.log(`--generate: put DOG's information in such order => First name, Breed, Age`);
+        console.log(`--produce: put CAT's information in such order => Nickname, Color, number of Siblings`);
+        console.log(`--discover: put TOY's information in such order => Type, Material, Size`);
         break;
 
-        case `--generate`:
-                let firstname = argv[1];
-                let breed = argv[2];
-                let age = argv[3];
-                let dogs = new Dog(firstname, breed, age);
-                dogs.ShowDog();
-            break;
+    case `--create`:
+        let name = argv[1];
+        let pass = argv[2];
+        let users = new User(name, pass);
+        users.ShowUser();
+        break;
 
-        case `--produce`:
-                let nickname = argv[1];
-                let color = argv[2];
-                let siblings = argv[3];
-                let cats = new Cat(nickname, color, siblings);
-                cats.ShowCat();
-            break;
-            
-        case `--discover`:
-                let type = argv[1];
-                let material = argv[2];
-                let size = argv[3];
-                let toys = new Toy(type, material, size);
-                toys.ShowToy();
-            break;
+    case `--generate`:
+        let firstname = argv[1];
+        let breed = argv[2];
+        let age = argv[3];
+        let dogs = new Dog(firstname, breed, age);
+        dogs.ShowDog();
+        break;
+
+    case `--produce`:
+        let nickname = argv[1];
+        let color = argv[2];
+        let siblings = argv[3];
+        let cats = new Cat(nickname, color, siblings);
+        cats.ShowCat();
+        break;
+
+    case `--discover`:
+        let type = argv[1];
+        let material = argv[2];
+        let size = argv[3];
+        let toys = new Toy(type, material, size);
+        toys.ShowToy();
+        break;
 
     default:
         console.log(`use --help To list all options`);
         break;
 }
 
-let Bill = new User("Bill", 555);
-Bill.ShowUser();
 
-let Jack = new Dog("Jack", "doberman", 5);
-Jack.ShowDog();
 
-let Nika = new Cat("Nika", "white", 3);
-Nika.ShowCat();
 
-let Jerry = new Toy("mouse", "wood", "big");
-Jerry.ShowToy();
+
 
